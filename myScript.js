5bacb5c678ac2eefe5db8f62babf22a767e5fadf
lanadian3lle <lanadianelle.n380@slcstudents.org>
2019-01-15 08:37:11 -0700

    I created a new project web[age.

---
+++ myScript.js
@@ -0,0 +1,22 @@
+//This function changes the text in the demo paragraph
+
+function myFunction() {
+    document.getElementById('demo').innerHTML = Paragraph Changed;
+    
+}
+//Defining variables
+//var x = 5;
+//var= y = 6;
+//var z = x + y;
+
+var x, y, z;
+x=5;
+y=6;
+z= x + y;
+
+//This function changes the text in the math paragraph
+
+function mathfunction () {
+ document.getElementById('math'). innerHTML=z;  
+    
+}
