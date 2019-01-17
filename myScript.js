a81a14167174cfdd2f3e236e24804d4f15bf2f56
lanadian3lle <lanadianelle.n380@slcstudents.org>
2019-01-17 08:27:35 -0700

    updated function

--- myScript.js
+++ myScript.js
@@ -4,19 +4,9 @@ function myFunction() {
     document.getElementById('demo').innerHTML = "Paragraph Changed";
     
 }
-//Defining variables
-//var x = 5;
-//var= y = 6;
-//var z = x + y;
-
-var x, y, z;
-x=5;
-y=6;
-z= x + y;
 
 //This function changes the text in the math paragraph
 
-function mathFunction () {
- document.getElementById('math'). innerHTML=z;  
-    
+function mathFunction(x, y) {
+return x + y   
 }
